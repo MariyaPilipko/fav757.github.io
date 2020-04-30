bfef86eea231bd6c2aaac0646ba23bc1a4c51354
Oleksandr Sapalsky <fav755@gmail.com>
2020-04-30 21:08:04 +0300

    Try to fix mobile bugs

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -21,102 +21,87 @@ const database = {
   ],
 };
 
+// Page elements variables
+const pageElements = {
+  topMenu: document.body.querySelector('.top-menu'),
+  heroCenterButton: document.getElementById('hero-zone-button'),
+  socialInfo: document.body.querySelector('.social-info'),
+  main : document.getElementById('main'),
+  content: document.querySelector('.content'),
+  buttonForArticles : document.body.querySelector('.content .fas, .fa-plus')
+}
+
 // Top menu change color on scroll
 document.addEventListener('scroll', function () {
-  const topMenu = document.body.querySelector('.top-menu');
-
   if (window.pageYOffset) {
-    topMenu.style.background = 'white';
-    topMenu.style.color = 'black';
-    topMenu.style.boxShadow = '0 2px 2px gray';
+    pageElements.topMenu.style.background = 'white';
+    pageElements.topMenu.style.color = 'black';
+    pageElements.topMenu.style.boxShadow = '0 2px 2px gray';
   } else {
-    topMenu.style.background = '';
-    topMenu.style.color = '';
-    topMenu.style.boxShadow = '';
+    pageElements.topMenu.style.background = '';
+    pageElements.topMenu.style.color = '';
+    pageElements.topMenu.style.boxShadow = '';
   }
 });
 
 // Header button on press scroll to main part
-document.getElementById('hero-zone-button').addEventListener('click', function () {
-  document.body.querySelector('.social-info').scrollIntoView({ behavior: "smooth" });
+pageElements.heroCenterButton.addEventListener('click', function () {
+  pageElements.socialInfo.scrollIntoView({ behavior: "smooth" });
 });
 
-// Describes behavior of articles element
-class ArticlesPreviews extends HTMLElement {
-  constructor() {
-    super();
+// Get article that is not used on the page
+function getNotUsedArticle() {
+  let notUserArticle;
 
-    // Create and style button for loading more articles
-    const buttonForMoreArticle = document.createElement('i');
-    buttonForMoreArticle.className = 'fas fa-plus';
-    buttonForMoreArticle.id = 'plus-button';
-
-    // Scroll article into view
-    buttonForMoreArticle.addEventListener('click', function () {
-      ArticlesPreviews.loadArticles();
-      buttonForMoreArticle.scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // Append button into the page
-    this.after(buttonForMoreArticle);
-
-    // Intial article load
-    ArticlesPreviews.loadArticles();
-  }
-
-  // Parent element for articles
-  static element = document.querySelector('.content');
-
-  // Get article that is not used on the page
-  static getNotUsedArticle() {
-    let notUserArticle;
-
-    for (let article of database.articles) {
-      if (!this.element.querySelector(`[name="${article.name}"]`)) {
-        notUserArticle = article;
-        return notUserArticle;
-      }
+  for (let article of database.articles) {
+    if (!pageElements.content.querySelector(`[name="${article.name}"]`)) {
+      notUserArticle = article;
+      return notUserArticle;
     }
-  }
-
-  // Load a new article
-  static async loadArticles() {
-    //get not used article text
-    const notUsedArticle = this.getNotUsedArticle();
-
-    // Get response from server with article html and add it on the page
-    const response = await fetch(`resources/articles/${notUsedArticle.name}.html`);
-    const htmlText = await response.text();
-
-    // Create article and add text to it
-    const article = document.createElement('article');
-    article.innerHTML = htmlText;
-
-    // Set name and class for article
-    article.setAttribute('name', notUsedArticle.name);
-    article.className = 'content_article';
-
-    // Open aricle event
-    const articleHeader = article.querySelector('h3');
-    articleHeader.addEventListener('click', (event) => {
-      const mainPart = document.getElementById('main');
-      const plusButton = document.getElementById('plus-button');
-
-      this.element.style.opacity = 0;
-
-      this.element.ontransitionend = () => {
-        this.element.style.display = 'none';
-        plusButton.style.display = 'none';
-        
-        this.element.style.opacity = 1;
-
-        mainPart.append(article);
-      }
-    });
-
-    //Push article
-    this.element.append(article);
   }
 }
 
-customElements.define('articles-preview', ArticlesPreviews);
+// Load a new article
+async function loadArticles() {
+  //get not used article text
+  const notUsedArticle = getNotUsedArticle();
+
+  // Get response from server with article html and add it on the page
+  const response = await fetch(`resources/articles/${notUsedArticle.name}.html`);
+  const htmlText = await response.text();
+
+  // Create article and add text to it
+  const article = document.createElement('article');
+  article.innerHTML = htmlText;
+
+  // Set name and class for article
+  article.setAttribute('name', notUsedArticle.name);
+  article.className = 'content_article';
+
+  // Open aricle event
+  const articleHeader = article.querySelector('h3');
+  articleHeader.addEventListener('click', (event) => {
+    const mainPart = document.getElementById('main');
+    const plusButton = document.getElementById('plus-button');
+
+    pageElements.content.style.opacity = 0;
+
+    pageElements.content.ontransitionend = () => {
+      pageElements.content.style.display = 'none';
+      plusButton.style.display = 'none';
+
+      pageElements.content.style.opacity = 1;
+
+      mainPart.append(article);
+    }
+  });
+
+  //Push article
+  pageElements.content.append(article);
+}
+
+// Event to load more articles
+pageElements.buttonForArticles.addEventListener('click', function () {
+  loadArticles();
+  pageElements.buttonForArticles.scrollIntoView({ behavior: 'smooth' });
+});
